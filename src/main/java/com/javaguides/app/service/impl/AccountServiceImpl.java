a26897618b60be9ccc5b5a04e044e6c741e7d315
Surendar <batthulasurendar@gmail.com>
2024-07-21 18:30:37 +0530

    code fixed

--- src/main/java/com/javaguides/app/service/impl/AccountServiceImpl.java
+++ src/main/java/com/javaguides/app/service/impl/AccountServiceImpl.java
@@ -28,7 +28,9 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public AccountDto getAccountById (Long id){
-        Account foundAccount = accountRepository.findById(id).get();
+        Account foundAccount = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exists"));
         return AccountMapper.mapToAccountDto(foundAccount);
     }
 
